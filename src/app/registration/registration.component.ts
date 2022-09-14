66edbfe5bca85eae726a7960fa50b5e5c7acd83b
sreejuss <52235351+sreejuss@users.noreply.github.com>
2022-09-14 12:38:59 +0530

    3 files modified, 1 added

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl,FormGroup} from '@angular/forms'
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
+  genders = ['male', 'female']
+  roles = ['admin','role x', 'role y']
+  signupForm:FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      'email': new FormControl(null),
+      'password': new FormControl(null),
+      'gender': new FormControl(null),
+      'role': new FormControl(null),
+    })
+  }
+
+  onSubmit(){
+    console.log(this.signupForm.value);
   }
 
 }
