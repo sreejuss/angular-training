776e074a38131de22937ae0a7a0675090e6a42b5
sreejuss <52235351+sreejuss@users.noreply.github.com>
2022-09-14 18:31:53 +0530

    1 file modified

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl,FormGroup,Validators} from '@angular/forms'
+import {FormControl,FormGroup,Validators,FormBuilder} from '@angular/forms'
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -9,19 +9,30 @@ export class RegistrationComponent implements OnInit {
   genders = ['male', 'female']
   roles = ['admin','role x', 'role y']
   signupForm:FormGroup;
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
-    this.signupForm = new FormGroup({
+
+
+    // this.signupForm = new FormGroup({
+    //   'email': new FormControl(null,[Validators.required,Validators.email]),
+    //   'password': new FormControl(null,Validators.required),
+    //   'gender': new FormControl(null,Validators.required),
+    //   'role': new FormControl(null,Validators.required),
+    // })
+    this.signupForm=this.fb.group({
       'email': new FormControl(null,[Validators.required,Validators.email]),
       'password': new FormControl(null,Validators.required),
       'gender': new FormControl(null,Validators.required),
       'role': new FormControl(null,Validators.required),
     })
+
   }
 
+  
+
   onSubmit(){
-    console.log(this.signupForm.value);
+    console.log(this.signupForm.get('email').value);
   }
 
 }
