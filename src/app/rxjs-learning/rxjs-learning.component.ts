11a0ec3bc21598334cc542f377d889424ee060cb
sreejuss <52235351+sreejuss@users.noreply.github.com>
2022-09-20 14:24:35 +0530

    3 files modified

--- src/app/rxjs-learning/rxjs-learning.component.ts
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Observable, of, debounceTime } from 'rxjs';
+import { Observable, of, debounceTime, fromEvent, interval, elementAt } from 'rxjs';
+import { filter, first, last, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs-learning',
@@ -9,26 +10,60 @@ import { Observable, of, debounceTime } from 'rxjs';
 })
 export class RxjsLearningComponent implements OnInit {
   searchForm:FormGroup;
-
-  student$: Observable<any> = of('hello');
-
+  // of operator
+  string$: Observable<any> = of('hello');
+  array$: Observable<any> = of(['Sreeju','John','Mark']);
+  object = {
+    id:10,
+    name:'sreeju'
+  }
+  object$: Observable<any> = of(this.object);
+  // end of of-operator
   constructor(private fb:FormBuilder) {}
-
+  // from event
+  @ViewChild('validate')
+  validate: ElementRef;
   ngOnInit() {
     this.searchForm=new FormGroup({
       name: new FormControl('')
     })
     this.searchForm.get('name').valueChanges.pipe(
-      debounceTime(400)
+      //take(2),
+      // elementAt(1),
+      // first(),
+      // last(),
+      //debounceTime(400)
+      filter((v)=>this.checkCondition(v))
     ).subscribe(data=>{
       console.log(data)
     })
-    this.student$.subscribe((res)=>{
+    this.string$.subscribe((res)=>{
+      console.log(res)
+    })
+    this.array$.subscribe(res=>{
+      const seqNum$ = interval(500);
+      seqNum$.subscribe(num=>{
+        if(num<5){
+          console.log(res + num)
+        }
+      })
+    })
+    this.object$.subscribe((res)=>{
       console.log(res)
     })
   }
 
+  checkCondition(v){
+    return v.length < 6? true: false;
+  }
+
   readValue(){
     console.log
+  }
+  fromEventObservable(){
+    const btnObservable$ = fromEvent(this.validate?.nativeElement,'click')
+    btnObservable$.subscribe(data=>{
+      console.log(data)
+    })
   }
 }
