a247fb8a2ef2c9197cf583a1d5bc30508a2c1b3a
sreejuss <52235351+sreejuss@users.noreply.github.com>
2022-09-28 11:33:04 +0530

    1 file modified, 2 added, 1 deleted

--- src/app/scss-learning/scss-learning.component.ts
+++ src/app/scss-learning/scss-learning.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-scss-learning',
   templateUrl: './scss-learning.component.html',
-  styleUrls: ['./scss-learning.component.css']
+  styleUrls: ['./scss-learning.component.scss'],
 })
 export class ScssLearningComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
